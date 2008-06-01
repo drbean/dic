16870785ac7f9202b69ff3c72e6d3debf84898b4
drbean <drbean@1c53d223-6d2d-dd11-bd8a-000bcdefe56b>
2008-06-01 03:20:58 +0000

    Work around unclozedhalf rigidity in View by adding Unclozeable for last letter while keeping it on published accessor.

--- lib/FirstLast.pm
+++ lib/FirstLast.pm
@@ -1,9 +1,7 @@
 package FirstLast;  # assumes Some/Module.pm
 
-# Last Edit: 2008 Apr 02, 09:17:36 PM
+# Last Edit: 2008 Jun 01, 10:26:31 AM
 # $Id$
-
-# http://www.mrc-cbu.cam.ac.uk/~mattd/Cmabrigde/
 
 use strict;
 use warnings;
@@ -11,8 +9,7 @@ use warnings;
 use base qw/LineParser/;
 
 use Parse::RecDescent;
-use Word;
-use Newline;
+use Element;
 
 =head1 NAME
 
@@ -24,14 +21,14 @@ See L<Cloze>
 
 =head1 DESCRIPTION
 
-Last letters of 2 letter words are also clozed. 1st letters of 1-letter words are not.
+Last letters of 2 letter words are also clozed. 1st letters of 1-letter words are not. See http://www.mrc-cbu.cam.ac.uk/~mattd/Cmabrigde/
+
 
 =cut
 
 =head2 parse
 
-Parse text and create cloze
-
+Parse text and create cloze. To work around dic's View having just one unclozed part coming before the clozed part, the Last letter is treated as a separate Unclozeable, except for the Dictionary and some trickery is done with published and clozed accessors.
 
 =cut
 
@@ -39,58 +36,81 @@ sub parse
 {
 	my $self = shift;
 	my $unclozeables = shift;
-        our $unclozeable = qr/(?:$unclozeables)/;
-	my $lines = shift;
+        our $unclozeable = $unclozeables? qr/(?:$unclozeables)/: undef;
+	our $lines = shift;
 	my @text = ();
-	our (%dic, @clozeline) = ();
+	our @clozeline = ();
+	our %dic = ();
 	my $letterGrammar = q[
 		{
 		my $punctuation = qr/[^A-Za-z0-9\\n]+/;
 		my $letter = qr/[A-Za-z0-9]/;
 		my $skip = '';
-		my ($cword, $fullword);
-		my ($middleletters, $inWord) = (0) x 2;
+		my $inWord = 0;
+		my ($first, $clozedletters, $offset, $length, $pretext);
 	}
 		string: token(s) end | <error>
 		token: newline | pass | firstletter | middleletter | lastletter | punctuation
 		newline: <reject: $inWord> "\\n" { push @FirstLast::clozeline, Newline->new }
-		pass: <reject: $inWord> m/($FirstLast::unclozeable|\\d+:\\d+)(?=$punctuation|$)/m
-			{ push @FirstLast::clozeline, $item[2]; }
 		firstletter: <reject: $inWord> m/$letter/ 
 			{
 				$inWord=1;
-				$cword = '';
-				push @FirstLast::clozeline, $item[2];
-				$fullword = $item[2];
+				$first = $item[2];
+				$clozedletters = '';
+				$offset = List::Util::max(0, $prevoffset-50);
+				$length = List::Util::min($prevoffset, 50);
+				$pretext = substr($FirstLast::lines, $offset, $length);
 			}
 		middleletter: <reject: not $inWord> m/$letter(?!$punctuation)/
 			{
-				$cword .= $item[2];
-				$fullword .= $item[2];
+				$clozedletters .= $item[2];
 			}
 		lastletter: <reject: not $inWord> m/$letter(?=$punctuation|$)/m
 			{
 				$inWord=0;
-				$middleletters = length($cword);
-				if ( $middleletters <= 0 )
-				{
-					$cword .= $item[2];
-					push @FirstLast::clozeline, 
-						Word->new($cword);
-				}
-				else
-				{				
-					push @FirstLast::clozeline, 
-						Word->new($cword);
-					push @FirstLast::clozeline, $item[2];
-				}
-				$fullword .= $item[2];
-				$FirstLast::dic{$fullword}++;
+				my $middle = length($clozedletters);
+				my $lastletter = $item[2];
+				my $clozedhalf = $clozedletters;
+				$clozedhalf .= $lastletter if $middle <= 0;
+				my $pub = $first . $clozedhalf;
+				$pub .= $lastletter unless $middle <= 0;
+				$FirstLast::dic{$pub}++;
+				my $posttext = substr($text,0,50);
+				push @FirstLast::clozeline, 
+					Word->new({
+						published => $pub,
+						unclozed => $first,
+						clozed => $clozedhalf,
+						pretext => $pretext,
+						posttext => $posttext
+					});
+				push @FirstLast::clozeline, Unclozeable->new({
+					published => $lastletter,})
+						unless ( $middle <= 0 );
 			}
 		punctuation: <reject: $inWord> m/$punctuation/
-			{ push @FirstLast::clozeline, $item[2]; }
+			{ push @FirstLast::clozeline, Unclozeable->new
+						({published => $item[2]});
+			}
 		end: m/^\Z/
 		]; 
+	if ( $unclozeables )
+	{
+		$letterGrammar .= q[
+		pass: <reject: $inWord> m/($FirstLast::unclozeable|$letter|\\d+:\\d+)(?=$punctuation|$)/m
+			{ push @FirstLast::clozeline, Unclozeable->new
+						({published => $item[2]});
+				$FirstLast::dic{$item[2]}++ if $item[2] =~m/^\w+$/;}
+		]; 
+	}
+	else {
+		$letterGrammar .= q[
+		pass: <reject: $inWord> m/($letter|\\d+:\\d+)(?=$punctuation|$)/m
+			{ push @FirstLast::clozeline, Unclozeable->new
+						({published => $item[2]});
+				$FirstLast::dic{$item[2]}++ if $item[2] =~m/^\w+$/;}
+		]; 
+	}
 	my $letterParser = Parse::RecDescent->new($letterGrammar);
 	defined $letterParser->string($lines) or die "letterparse NOK.";
 	return bless { dictionary => \%FirstLast::dic,
