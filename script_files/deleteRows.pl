d1c001b94650f419ded4efc63de970e93d560924
drbean <drbean@1c53d223-6d2d-dd11-bd8a-000bcdefe56b>
2009-03-30 04:13:46 +0000

    r858@localhost:  drbean | 2008-11-25 08:17:59 +0800  'callback' from commandline ARGV[1] =~/eq/ne/gt ARGV[3]

--- script_files/deleteRows.pl
+++ script_files/deleteRows.pl
@@ -6,13 +6,14 @@ deleteRows.pl - Emulate cli db tool, dbtool.pl with DB schema
 
 =head1 SYNOPSIS
 
-script_files/deleteRows.pl Player name Jack
-95801001 Jack
-95801007 Jack
+script_files/deleteRows.pl Player player =~/eq/ne/gt ^19300
+Columns:        league  exercise        player  question        answer  correct
+=============================================
+Deleting        dic     crocodile1      193001  3       Don't know      0
 
 =head1 DESCRIPTION
 
-Deletes all rows from the table associated with the Player schema which have Jack in the name column. Delete ALL rows if no column name and value. Not using DBIC::ResultSet's delete_all, because play needs to be kept even after players are gone. Players will come back.
+Deletes all rows from the table associated with the ARGV[0] schema for which sub { ARGV[0] ARGV[2] ARGV[3] } returns true. Now using DBIC::Row's delete. Before was NOT using DBIC::ResultSet's delete_all, because play needs to be kept even after players are gone. Players will come back.
 
 =head1 AUTHOR
 
@@ -46,8 +47,32 @@ my $modelmodule = "${name}::Model::DB";
 my $connect_info = $modelmodule->config->{connect_info};
 my $d = $model->connect( @$connect_info );
 my $s;
-$s = $d->resultset($ARGV[0])->search( { $ARGV[1] => $ARGV[2] } ) if @ARGV==3;
-$s = $d->resultset($ARGV[0]) if @ARGV==1;
-my $deletions = $s->count;
-print "Deleting $deletions rows ...\n";
-$s->delete;
+$s = $d->resultset(shift @ARGV);
+my @columns = $s->result_source->columns;
+$, = "\t";
+print "Columns: ", @columns,
+	"\n=============================================\n";
+my $callback = callback( @ARGV );
+while ( my $r = $s->next )
+{
+	my %values = map { $_ => $r->get_column($_) } @columns;
+	if ( $callback->( %values ) )
+	{
+		$r->delete;
+		print "Deleting";
+		print "\t" . $values{$_} for @columns;
+		print "\n";
+	}
+}
+
+sub callback {
+	my @token = @_;
+	my $regex = qr/$token[2]/;
+	my %callbacks = (
+		'=~' => sub { my %r=@_; return ( $r{$token[0]} =~ $regex ) },
+		'eq' => sub { my %r=@_; return ( $r{$token[0]} eq $token[2] ) },
+		'ne' => sub { my %r=@_; return ( $r{$token[0]} ne $token[2] ) },
+		'gt' => sub { my %r=@_; return ( $r{$token[0]} gt $token[2] ) },
+		);
+	return $callbacks{$token[1]};
+}
