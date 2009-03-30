0d952cc53ed34aac49596d1c20b1390eafbc2661
drbean <drbean@1c53d223-6d2d-dd11-bd8a-000bcdefe56b>
2009-03-30 04:17:08 +0000

    r1116@localhost:  drbean | 2009-03-01 10:13:51 +0800  Build $players->{league} field in for loop using league.yaml

--- script_files/playerleagues.pl
+++ script_files/playerleagues.pl
@@ -253,10 +253,6 @@ N9561761	林家伶	jia
 N9561764	郭政勳	zheng
 CLA0013
 
-$leaguefile = LoadFile "/home/drbean/class/FLA0015/league.yaml";
-push @{$players->{FLA0015}}, map {[ $_->{id}, $_->{Chinese}, $_->{password} ]}
-				@{$leaguefile->{member}};
-
 push @{$players->{FLB0002}}, [split] for <<FLB0002 =~ m/^.*$/gm;
 N9361738 江映霖	ying
 N9361749 覃少穎	shao
@@ -285,9 +281,11 @@ N9461766 劉毓汶	yu
 U9533039 蕭郁玲	yu
 FLB0002
 
-$leaguefile = LoadFile "/home/drbean/class/MIA0012/league.yaml";
-push @{$players->{MIA0012}}, map {[ $_->{id}, $_->{Chinese}, $_->{password} ]}
-				@{$leaguefile->{member}};
+for my $league ( 'FLA0015', 'MIA0012' ) {
+	$leaguefile = LoadFile "/home/drbean/class/$league/league.yaml";
+	push @{$players->{$league}}, map {[ $_->{id}, $_->{Chinese}, $_->{password} ]}
+					@{$leaguefile->{member}};
+}
 
 push @{$players->{access}}, [split] for <<ACCESS =~ m/^.*$/gm;
 U9424017	黃季雯	Ji
