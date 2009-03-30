223321fe7f5a8f60c2fb8bbd3b96f74b8592411d
drbean <drbean@1c53d223-6d2d-dd11-bd8a-000bcdefe56b>
2009-03-30 04:17:42 +0000

    r1160@localhost:  drbean | 2009-03-17 08:36:22 +0800  merged r1159 (only list exercises in league's exercise table) by switching from target. Actually, not target-related code

--- script_files/scores.pl
+++ script_files/scores.pl
@@ -66,8 +66,18 @@ print "In $dir directory:\n";
 my $scores;
 for my $id ( sort @leagueids )
 {
-	my @leagueExercises = @exerciseIds;
-	push @leagueExercises, @newExerciseList if $dir eq $id and $league;
+	# my @leagueExercises = @exerciseIds;
+	my @leagueExercises;
+	if ( $dir eq $id and $league ) {
+		push @leagueExercises, @newExerciseList;
+	}
+	elsif ( $dir eq 'dic' or $dir eq 'target' or $dir eq 'access' ) {
+		my $league = $schema->resultset('League')->find({ id => $dir });
+		my $genre = $league->genre->get_column('genre') if $league;
+		my @newExerciseList = uniq $schema->resultset('Exercise')
+			->search({ genre => $genre })->get_column('id')->all;
+		push @leagueExercises, @newExerciseList;
+	}
 	@leagueExercises = uniq @leagueExercises;
 	print $id . "\t", @leagueExercises , "Total\n";
 	print "============================================\n";
