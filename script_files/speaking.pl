cd6fa710dbe0c86e44577198b2f02b5ee1c0e6a3
drbean <drbean@1c53d223-6d2d-dd11-bd8a-000bcdefe56b>
2009-03-30 04:16:41 +0000

    r1105@localhost:  drbean | 2009-02-28 13:28:00 +0800  chasm. Why isn't Ctest passing times?

--- script_files/speaking.pl
+++ script_files/speaking.pl
@@ -23,13 +23,14 @@ my $schema = "${::name}::Schema"->connect( @$connect_info );
 use strict;
 
 my $texts = [
-	[ qw(id description genre content unclozeables) ],
+	[ qw(id description genre target content unclozeables) ],
 
 	[
         "tate",
         "Ed Tate's three speaking tips",
 	"speaking",
-	"OO:30 Griffin: I suppose this is a stupid question, but ..
+	"all",
+	"00:30 Griffin: I suppose this is a stupid question, but ..
 00:32 What is, like if you had to say, the three most important things that public speaking, uh, public speakers should do to, to excel?
 00:41 Tate: Well, I think there are several mistakes that .. I focus on business presenters .. and what I see in a business environment are several things.
 00:48 Number one, they focus on their, their computers and their content.
@@ -49,20 +50,64 @@ So that's something else to focus on, not so much on your expertise, but focus o
 	
 	];
 
-uptodatepopulate( 'Text', $texts );
-
 my $questions = [
-			[ qw/genre text id content answer/ ],
+			[ qw/genre text id target content answer/ ],
 
-[ "speaking", "tate", 1, "Tate says to focus on the audience as much as on the subject.", "True" ],
-[ "speaking", "tate", 2, "Tate says speakers need to be mindful of different things people focus on.", "True" ],
-[ "speaking", "tate", 3, "Tate says presenters need to focus on what the audience thinks, not what the speaker says.", "True" ],
-[ "speaking", "tate", 4, "Tate thinks speakers don't spend enough time focusing on their computers and content.", "False" ],
-[ "speaking", "tate", 5, "Tate says speakers think too much about different learning styles.", "False" ],
-[ "speaking", "tate", 6, "Tate says speakers need to be more mindful of their speaking style and their expertise as speakers.", "False" ],
+[ "speaking", "tate", 1, "all", "Tate says to focus on the audience as much as on the subject.", "True" ],
+[ "speaking", "tate", 2, "all", "Tate says speakers need to be mindful of different things people focus on.", "True" ],
+[ "speaking", "tate", 3, "all", "Tate says presenters need to focus on what the audience thinks, not what the speaker says.", "True" ],
+[ "speaking", "tate", 4, "all", "Tate thinks speakers don't spend enough time focusing on their computers and content.", "False" ],
+[ "speaking", "tate", 5, "all", "Tate says speakers think too much about different learning styles.", "False" ],
+[ "speaking", "tate", 6, "all", "Tate says speakers need to be more mindful of their speaking style and their expertise as speakers.", "False" ],
 
 	];
+push @$texts, [
+        "chasm",
+        "The effect of mispronouncing 'chasm'",
+	"speaking",
+	"all",
+	"1:26 Winograd really has a beautiful voice, resonant pitch, crystal-clear diction, good rate.
+1:30 You could tell from that clip that she was right on with all the aspects of her delivery.
+1:35 But you probably caught her mistake as quickly as I did.
+1:39 And I'm betting that you cringed, just as I did.
 
+...
+
+2:06 But the bigger question is, why should such a simple error have such a huge impact on my perception of her.
+2:12 Everyone makes mistakes, right?
+2:14 It doesn't mean she's completely stupid, right?
+2:17 Well, unfortunately, it caused me, and very probably many others, to have a reflex action that said, precisely that.
+2:25 It comes down to what we've been talking about for the last few weeks. Credibility.
+2:31 Mispronouncing a word is a mistake that is most often made by children. Therefore we associate mispronunciations with immaturity, not credibility.
+
+...
+
+3:13 So, how do you avoid this. Like I mentioned before, anyone can make a mistake. 
+3:19 Here's how to make sure you don't mistake pronunciation.
+3:24 When you write a speech, be sure that you write from your own vocabulary base, using words that you are familiar with.
+3:33 If you do introduce a new word, just make sure you look it up before you use it.
+3:37 You might be saying, Well, Winograd's comments sounded off-the-cuff. They weren't scripted beforehand. So how do you avoid mispronunciation mistakes in the moment?
+3:46 Well, I doubt that her comments were off-the-cuff. When people are speaking extemperaneously, they rarely use words that aren't a part of their natural language pattern.
+
+4:00 And the perils of not practicing. It would have only taken one person to hear the mispronunciation to correct it.
+4:08 If they had and if she had pronounced the word correctly, who knows, she might have won some more votes.
+
+",
+"1:26|1:30|1:35|1:39|2:06|2:12|2:14|2:17|2:25|2:31|3:13|3:19|3:24|3:37|3:46|4:00|4:08|Winograd|resonant|crystal|diction|clip|cringed|reflex action|Credibility|immaturity|off-the-cuff|scripted|extemperaneously"
+	];
+
+push @$questions,
+
+[ "speaking", "chasm", 1, "all", "Mispronouncing a word might mean people perceive you as stupid.", "True" ],
+[ "speaking", "chasm", 2, "all", "To avoid pronunciation mistakes, you should practice before speaking.", "True" ],
+[ "speaking", "chasm", 3, "all", "You might make mispronunciation mistakes if you use words you don't know.", "True" ],
+[ "speaking", "chasm", 4, "all", "Everyone makes mistakes, so people don't think you are stupid if you mispronounce words.", "False" ],
+[ "speaking", "chasm", 5, "all", "To avoid pronunciation mistakes, Winograd should have written her words down.", "False" ],
+[ "speaking", "chasm", 6, "all", "Practicing beforehand with other people will not cause you to correct your mistakes.", "False" ];
+
+
+
+uptodatepopulate( 'Text', $texts );
 uptodatepopulate( 'Question', $questions );
 
 sub uptodatepopulate
