115c6d288da1c52375f925eb2e1162a3acc53901
drbean <drbean@1c53d223-6d2d-dd11-bd8a-000bcdefe56b>
2009-03-30 04:15:23 +0000

    r1062@localhost:  drbean | 2009-02-16 13:34:19 +0800  Must be players in hash prepared for db entry, apparently. So delete from leagueIds array

--- script_files/playerleagues.pl
+++ script_files/playerleagues.pl
@@ -19,7 +19,7 @@ BEGIN {
 	require "$::name/Schema.pm"; $::name->import;
 }
 
-my @leagueids = qw/GL00003 GL00022 GL00031 CLA FLA0015 FLB0002 access visitors/;
+my @leagueids = qw/GL00003 GL00022 CLA FLA0015 access visitors/;
 my $dir = ( File::Spec->splitdir(getcwd) )[-1];
 $dir = qr/^(GL000|CLA|FLA)/ if $dir eq 'dic';
 @leagueids = grep m/$dir/, @leagueids;
