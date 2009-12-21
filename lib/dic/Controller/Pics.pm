9d4d686c919f50295539f6ed90a6f4b69b0b79f0
drbean <drbean@1c53d223-6d2d-dd11-bd8a-000bcdefe56b>
2009-12-21 12:35:11 +0000

    r1480@e_learning:  drbean | 2009-05-30 20:40:25 +0800  Move tag munging (but not stemming) after finding oldurls. Forgot to get more photos from flickr servers until number eq needed. No owner in Pic, use represented hash to get only picture per owner. Populate Pic after needed pics gotten. So back out owner column in Pic

--- lib/dic/Controller/Pics.pm
+++ lib/dic/Controller/Pics.pm
@@ -23,8 +23,8 @@ Catalyst Controller.
 
 =head2 find
 
-Find a Flickr picture. Would be good to be able to hit database only once for all the pictures with one tag. Perhaps I should do that when building the exercise. Doing it JIT, need to instantiate API object each time.
-	$c->stash->{url} = 'http://farm4.static.flickr.com/3515/3470432168_8e8509962d.jpg';
+Find a needed number of Flickr picture.
+$c->stash->{url} = 'http://farm4.static.flickr.com/3515/3470432168_8e8509962d.jpg';
 
 =cut
  
@@ -38,53 +38,57 @@ sub find : Local {
 	my $tag = $word->published;
 	my $lctag = lc $tag;
 	my $stem = Lingua::Stem::stem($lctag)->[0];
-	my $canonize = Lingua::EN::Infinitive->new;
-	my @infinit;
-	for ( ($canonize->stem($lctag))[0..1] ) { push @infinit, $_ if $_ }
-	my @conjugates = map {( past($_), participle($_), gerund($_))} @infinit;
-	my $tags = join ',', $tag, $lctag, @infinit, @conjugates;
-	my $titlesearch = join '|', $lctag, @infinit, @conjugates;
-	my $titleregex = qr/$titlesearch/i;
+	my $pics = $c->model('DB::Pic');
 	$c->stash->{exerciseid} = $word->exercise->id;
 	$c->stash->{title} = $word->exercise->description;
-	my $pics = $c->model('DB::Pic');
 	$c->stash->{template} = 'pics/list.tt2';
-	my $fetched = 300;
-	my $needed = 20;
-	my $page = 1;
 	my @oldurls = $pics->search({ word => $stem });
 	unless ( @oldurls ) {
+		my $canonize = Lingua::EN::Infinitive->new;
+		my @infinit;
+		for ( ($canonize->stem($lctag))[0..1] )
+		{ push @infinit, $_ if $_ }
+		my @conjugates = map
+			{( past($_), participle($_), gerund($_))} @infinit;
+		my $tags = join ',', $tag, $lctag, @infinit, @conjugates;
+		my $titlesearch = join '|', $lctag, @infinit, @conjugates;
+		my $titleregex = qr/$titlesearch/i;
+		my $fetched = 300;
+		my $needed = 40;
+		my $page = 1;
 		my $api = Flickr::API->new({key =>
 			'ea697995b421c0532215e4a2cbadbe1e',
 			secret => 'ab2024b750a9d1f2' });
-		my $r = $api->execute_method('flickr.photos.search',
-			{ tags => $tags, per_page => $fetched,
-				page => $page++, api_key =>
-				'ea697995b421c0532215e4a2cbadbe1e' });
-		unless ( $r->{success} ) {
-			$c->stash->{error_msg} = $r->{error_message};
-			return;
+		my (@newurls, %represented);
+		while ( $needed >= 0 ) {
+			my $r = $api->execute_method('flickr.photos.search',
+				{ tags => $tags, per_page => $fetched,
+					page => $page++, api_key =>
+					'ea697995b421c0532215e4a2cbadbe1e' });
+			unless ( $r->{success} ) {
+				$c->stash->{error_msg} = $r->{error_message};
+				return;
+			}
+			for my $n ( 0 .. $fetched-1 ) {
+				my $photo = $r->{tree}->{children}->[1]->
+					{children}->[2*$n+1]->{attributes};
+				next unless $photo->{title} =~ $titleregex;
+				my $owner = $photo->{owner};
+				next if $represented{$owner};
+				$represented{$owner}++;
+				my %row;
+				$row{title} = $photo->{title};
+				$row{id} = undef;
+				$row{word} = $stem;
+				$row{url} = 'http://farm' . $photo->{farm} .
+					'.static.flickr.com/'.  $photo->
+					{server} .  '/'.  $photo->{id} . '_' .
+					$photo->{secret} . '_t.jpg';
+				push @newurls, \%row;
+				$needed--;
+			}
 		}
-		my @newurls;
-		for my $n ( 0 .. $fetched-1 ) {
-			my $photo = $r->{tree}->{children}->[1]->
-				{children}->[2*$n+1]->{attributes};
-			next unless $photo->{title} =~ $titleregex;
-			my $owner = $photo->{owner};
-			next if $pics->search({ owner => $owner })->count;
-			my %row;
-			$row{title} = $photo->{title};
-			$row{id} = undef;
-			$row{word} = $stem;
-			$row{owner} = $owner;
-			$row{url} = 'http://farm' . $photo->{farm} .
-				'.static.flickr.com/'.  $photo->
-				{server} .  '/'.  $photo->{id} . '_' .
-				$photo->{secret} . '_t.jpg';
-			$pics->update_or_create( \%row );
-			push @newurls, \%row;
-			$needed--;
-		}
+		$pics->populate( \@newurls );
 		$c->stash->{urls} = \@newurls;
 	}
 	else { $c->stash->{urls} = \@oldurls; }
