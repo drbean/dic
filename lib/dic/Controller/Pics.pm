6614cede52d97dc4f9f4c2ffb6a0e836ed504081
drbean <drbean@1c53d223-6d2d-dd11-bd8a-000bcdefe56b>
2009-12-21 12:35:43 +0000

    r1710@e_learning:  drbean | 2009-06-30 18:15:39 +0800  Get around ban on flickr. Download the jpgs and store on web.nuu.edu.tw. But HTTP::Response's decoded_content seems to corrupt the jpg. And sts=4

--- lib/dic/Controller/Pics.pm
+++ lib/dic/Controller/Pics.pm
@@ -4,6 +4,8 @@ use Moose;
 BEGIN { extends 'Catalyst::Controller'; }
 
 use Flickr::API;
+use Net::FTP;
+use IO::All;
 use Lingua::Stem;
 use Lingua::EN::Infinitive;
 use Lingua::EN::Conjugate qw/past participle gerund/;
@@ -29,71 +31,100 @@ $c->stash->{url} = 'http://farm4.static.flickr.com/3515/3470432168_8e8509962d.jp
 =cut
  
 sub find : Local {
-	my ($self, $c, $exerciseId, $wordId) = @_;
-	my $league = $c->session->{league};
-	my $genre = $c->model("DB::Leaguegenre")->find
-			( {league => $league} )->genre;
-	my $word = $c->model('DB::Word')->find({exercise=>$exerciseId,
-			genre => $genre, id => $wordId });
-	my $tag = $word->published;
-	my $lctag = lc $tag;
-	my $stem = Lingua::Stem::stem($lctag)->[0];
-	my $pics = $c->model('DB::Pic');
-	$c->stash->{exerciseid} = $word->exercise->id;
-	$c->stash->{title} = $word->exercise->description;
-	$c->stash->{template} = 'pics/list.tt2';
-	my @oldurls = $pics->search({ word => $stem });
-	unless ( @oldurls ) {
-		my $canonize = Lingua::EN::Infinitive->new;
-		my @infinit;
-		for ( ($canonize->stem($lctag))[0..1] )
-		{ push @infinit, $_ if $_ }
-		my @conjugates = map
-			{( past($_), participle($_), gerund($_))} @infinit;
-		my $tags = join ',', $tag, $lctag, @infinit, @conjugates;
-		my $titlesearch = join '|', $lctag, @infinit, @conjugates;
-		my $titleregex = qr/$titlesearch/i;
-		my $fetched = 300;
-		my $needed = 40;
-		my $page = 1;
-		my $api = Flickr::API->new({key =>
-			'ea697995b421c0532215e4a2cbadbe1e',
-			secret => 'ab2024b750a9d1f2' });
-		my (@newurls, %represented);
-		while ( $needed >= 0 ) {
-			my $r = $api->execute_method('flickr.photos.search',
-				{ tags => $tags, per_page => $fetched,
-					page => $page++, api_key =>
-					'ea697995b421c0532215e4a2cbadbe1e' });
-			unless ( $r->{success} ) {
-				$c->stash->{error_msg} = $r->{error_message};
-				return;
-			}
-			for my $n ( 0 .. $fetched-1 ) {
-				my $photo = $r->{tree}->{children}->[1]->
-					{children}->[2*$n+1]->{attributes};
-				next unless $photo->{title} =~ $titleregex;
-				my $owner = $photo->{owner};
-				next if $represented{$owner};
-				$represented{$owner}++;
-				my %row;
-				$row{title} = $photo->{title};
-				$row{id} = undef;
-				$row{word} = $stem;
-				$row{url} = 'http://farm' . $photo->{farm} .
-					'.static.flickr.com/'.  $photo->
-					{server} .  '/'.  $photo->{id} . '_' .
-					$photo->{secret} . '_t.jpg';
-				push @newurls, \%row;
-				$needed--;
-			}
-		}
-		$pics->populate( \@newurls );
-		$c->stash->{urls} = \@newurls;
-	}
-	else { $c->stash->{urls} = \@oldurls; }
-}
+    my ( $self, $c, $exerciseId, $wordId ) = @_;
+    my $league = $c->session->{league};
+    my $genre =
+      $c->model("DB::Leaguegenre")->find( { league => $league } )->genre;
+    my $word = $c->model('DB::Word')->find(
+        {
+            exercise => $exerciseId,
+            genre    => $genre,
+            id       => $wordId
+        }
+    );
+    my $tag   = $word->published;
+    my $lctag = lc $tag;
+    my $stem  = Lingua::Stem::stem($lctag)->[0];
+    my $pics  = $c->model('DB::Pic');
+    $c->stash->{exerciseid} = $word->exercise->id;
+    $c->stash->{title}      = $word->exercise->description;
+    $c->stash->{template}   = 'pics/list.tt2';
+    my @oldurls = $pics->search( { word => $stem } );
 
+    unless (@oldurls) {
+        my $canonize = Lingua::EN::Infinitive->new;
+        my @infinit;
+        for ( ( $canonize->stem($lctag) )[ 0 .. 1 ] ) {
+            push @infinit, $_
+              if $_;
+        }
+        my @conjugates =
+          map { ( past($_), participle($_), gerund($_) ) } @infinit;
+        my $tags = join ',', $tag, $lctag, @infinit, @conjugates;
+        my $titlesearch = join '|', $lctag, @infinit, @conjugates;
+        my $titleregex = qr/$titlesearch/i;
+        my $fetched    = 300;
+        my $needed     = 40;
+        my $page       = 1;
+        my $api        = Flickr::API->new(
+            {
+                key    => 'ea697995b421c0532215e4a2cbadbe1e',
+                secret => 'ab2024b750a9d1f2'
+            }
+        );
+        my $ftp = Net::FTP->new('web.nuu.edu.tw') or die "web.nuu? $@";
+        $ftp->login( 'greg', '1514' ) or die "web.nuu.edu.tw login? $@";
+        $ftp->cwd('public_html/pics') or die "web/~greg/pics? $@";
+        my ( @newurls, %represented );
+        while ( $needed >= 0 ) {
+            my $r = $api->execute_method(
+                'flickr.photos.search',
+                {
+                    tags     => $tags,
+                    per_page => $fetched,
+                    page     => $page++,
+                    api_key  => 'ea697995b421c0532215e4a2cbadbe1e'
+                }
+            );
+            unless ( $r->{success} ) {
+                $c->stash->{error_msg} = $r->{error_message};
+                return;
+            }
+            for my $n ( 0 .. $fetched - 1 ) {
+                my $photo =
+                  $r->{tree}->{children}->[1]->{children}->[ 2 * $n + 1 ]
+                  ->{attributes};
+                next unless $photo->{title} =~ $titleregex;
+                my $owner = $photo->{owner};
+                next if $represented{$owner};
+                $represented{$owner}++;
+                my %row;
+                $row{title} = $photo->{title};
+                $row{id}    = undef;
+                $row{word}  = $stem;
+                $row{url} =
+                    'http://farm'
+                  . $photo->{farm}
+                  . '.static.flickr.com/'
+                  . $photo->{server} . '/'
+                  . $photo->{id} . '_'
+                  . $photo->{secret}
+                  . '_t.jpg';
+                my $remote = "$photo->{id}_$photo->{secret}_t.jpg";
+                my $local = "/tmp/pics/$remote";
+                my $pic = $api->get( $row{url} );
+                io( $local )->print( $pic->decoded_content ).
+                $ftp->put( $local ) or die "$pic pic on web.nuu? $@";
+                $row{url} = "http://web.nuu.edu.tw/~greg/pics/$remote";
+                push @newurls, \%row;
+                $needed--;
+            }
+        }
+        $pics->populate( \@newurls );
+        $c->stash->{urls} = \@newurls;
+    }
+    else { $c->stash->{urls} = \@oldurls; }
+}
 
 =cut
 
@@ -113,5 +144,8 @@ This library is free software, you can redistribute it and/or modify
 it under the same terms as Perl itself.
 
 =cut
+
+# vim: set ts=8 sts=4 sw=4 noet:
+#
 
 1;
