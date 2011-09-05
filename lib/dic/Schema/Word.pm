56d58c5b59cad077f17dc01e86ecf4dd24111712
drbean <drbean@1c53d223-6d2d-dd11-bd8a-000bcdefe56b>
2011-09-05 02:31:20 +0000

    problematic relation of Word and Dictionary, belongs_to Dictionary, so has_many? But might_have error is: not present in dictionaries.

--- lib/dic/Schema/Word.pm
+++ lib/dic/Schema/Word.pm
@@ -63,7 +63,7 @@ __PACKAGE__->set_primary_key("genre", "exercise", "target", "id");
 #     3) Column name in *this* table
 __PACKAGE__->belongs_to(exercise => 'dic::Schema::Exercise',
         {'foreign.genre' => 'self.genre', 'foreign.id' =>'self.exercise'});
-__PACKAGE__->might_have(dictionary => 'dic::Schema::Dictionary',
+__PACKAGE__->belongs_to(dictionary => 'dic::Schema::Dictionary',
         {'foreign.genre' => 'self.genre', 'foreign.word' =>'self.published'});
 
 
