961983d9e61c1d9eab83a5b30e66e61fa1937e6d
drbean <drbean@1c53d223-6d2d-dd11-bd8a-000bcdefe56b>
2011-09-22 08:11:59 +0000

    001

--- script_files/flier.pl
+++ script_files/flier.pl
@@ -10,7 +10,7 @@ my $sender = Email::Send->new({ mailer => 'SMTP' });
 $sender->mailer_args([ 
 	   Host     => 'email.nuu.edu.tw', # defaults to localhost
 	   username => 'greg',
-	   password => '1949',
+	   password => '',
 ]);
 
 my $tmpl = Text::Template->new( TYPE => 'FILE', SOURCE => 'flier1.tmpl' );
@@ -19,9 +19,9 @@ my $message = $tmpl->fill_in( hash => $datahash );
 
 $sender->send( $message );
 
-my @leagues = qw/MIA0009 BMA0076 BMA0077/;
+my @leagues = qw/BMA0031/;
 for my $league ( @leagues ) {
-	my $yaml = LoadFile "../records/982$league/league.yaml";
+	my $yaml = LoadFile "../../001/$league/league.yaml";
 	my $members = $yaml->{member};
 	my @ids = map { $_->{id} } @$members;
 	for my $id ( @ids ) {
