f08fc0c84423009924a542af82617f73a24845e8
drbean <drbean@1c53d223-6d2d-dd11-bd8a-000bcdefe56b>
2013-02-19 00:50:45 +0000

    perl script_files/delete_exercise exId1 exId2 ..

---
+++ script_files/delete_exercise.pl
@@ -0,0 +1,82 @@
+#!/usr/bin/perl 
+
+# Created: 西元2010年11月30日 09時59分46秒
+# Last Edit: 2012 Dec 11, 06:29:01 PM
+# $Id$
+
+=head1 NAME
+
+delete_exercise.pl - Delete exercise from database
+
+=cut
+
+use strict;
+use warnings;
+use FindBin qw/$Bin/;
+use lib "$Bin/../lib";
+
+use dic;
+use dic::Model::DB;
+my $connect_info = dic::Model::DB->config->{connect_info};
+my $schema = dic::Schema->connect( @$connect_info );
+
+=head1 SYNOPSIS
+
+perl script_files/delete_exercise exId1 exId2 ..
+
+=cut
+
+sub run {
+    my @exIds = @ARGV;
+    for my $exId ( @exIds ) {
+	my $exs = $schema->resultset('Exercise')->search( { id=>$exId } );
+	while ( my $ex = $exs->next ) {
+	    my $genre = $ex->genre;
+	    # my $dictionary = $ex->dictionary;
+	    my $words = $ex->words;
+	    my %entries;
+	    #while (my $word = $words->next)
+	    #{
+	    #        my $token = $word->published;
+	    #        my $entry = $dictionary->find({ word => $token });
+	    #        if ( $entry )
+	    #        {
+	    #    	    my $count = $entry->count;
+	    #    	    $entry->update( {count => --$count} );
+	    #        }
+	    #}
+	    $ex->questions->delete_all;
+	    $ex->delete;
+	}
+    }
+}
+
+run unless caller;
+
+
+=head1 DESCRIPTION
+
+Delete an exercise. Delete of Text, Questions and Questionwords done here too.
+
+=cut
+
+=head1 AUTHOR
+
+Dr Bean C<< <drbean at cpan, then a dot, (.), and org> >>
+
+=cut
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2010 Dr Bean, all rights reserved.
+
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
+=cut
+
+# End of create_exercise.pl
+
+# vim: set ts=8 sts=4 sw=4 noet:
+
+
