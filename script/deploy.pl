72bf5fd4d2e73bb32366327c0fd1d7f65c48c184
drbean <drbean@1c53d223-6d2d-dd11-bd8a-000bcdefe56b>
2013-08-16 13:18:31 +0000

    dic::Model::DB,dic::Schema,config->{connect_info}

--- script/deploy.pl
+++ script/deploy.pl
@@ -8,21 +8,11 @@ use Config::General;
 use Cwd;
 use File::Spec;
 
-BEGIN {
-	my @MyAppConf = glob( '*.conf' );
-	die "Which of @MyAppConf is the configuration file?"
-				unless @MyAppConf == 1;
-	my %config = Config::General->new($MyAppConf[0])->getall;
-	$::name = $config{name};
-	require "$::name.pm"; $::name->import;
-	require "$::name/Schema.pm"; $::name->import;
-}
+use dic::Model::DB;
+use dic::Schema;
 
-no strict qw/subs refs/;
-my $connect_info = "${::name}::Model::DB"->config->{connect_info};
-# my $connect_info = [ 'dbi:SQLite:db/demo','','' ];
-my $schema = "${::name}::Schema"->connect( @$connect_info );
-use strict;
+my $connect_info = dic::Model::DB->config->{connect_info};
+my $schema = dic::Schema->connect( $connect_info );
 
 $schema->deploy;
 
